9b5274e29d5d7bec86a3b5082230fa755bab8814
Rick Christenhusz <rick.christenhusz@gmail.com>
2018-10-25 16:34:59 -0400

    Updated Schema variable assignment

--- models/users.js
+++ models/users.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const userSchema = Schema({
+const userSchema = new mongoose.Schema({
     username: String,
     password: String
 })
