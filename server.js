1eaa949dfeeaffdc09813119df8b9db92d4e98f2
Michael Merlo <michaelmerlo@Michaels-MBP.home>
2018-10-25 15:08:57 -0400

    Server.js created, functioning, cars controller created, routes created in carsController

---
+++ server.js
@@ -0,0 +1,28 @@
+// npm init
+// npm i express
+//npm i mongoose
+
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose')
+
+
+app.use(express.json());
+app.use(express.static('public'));
+
+
+const CarsController = require('./controllers/cars.js')
+app.use('/cars', CarsController)
+
+
+mongoose.connect('mongodb://localhost:27017/movieCars', { useNewUrlParser: true});
+mongoose.connection.once('open', ()=>{
+    console.log('connected to mongoose...');
+});
+
+
+
+
+app.listen(3000, ()=>{
+    console.log('listening...');
+});
