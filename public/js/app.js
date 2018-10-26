00014e32df292bc2478d0b899a73b42ec4bfc839
Michael Merlo <michaelmerlo@Michaels-MBP.home>
2018-10-26 19:05:24 -0400

    added in app.js file

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,44 @@
-console.log('Hello World');
+const app = angular.module('MovieCars', [])
+
+app.controller('MovieCarController', ['$http', function ($http) {
+
+    this.logIn = function(){
+        $http({
+            method:'POST',
+            url:'/sessions',
+            data: {
+                username:this.username,
+                password:this.password
+            }
+        }).then(function(response){
+            console.log(response);
+        })
+    }
+
+    const controller = this;
+    this.getCars = function(){
+        $http({
+            method:'GET',
+            url:'/app'
+        }).then(function(response){
+            controller.loggedInUsername = response.data.username;
+        })
+    }
+
+
+
+  this.createUser = function(){
+      $http({
+          method:'POST',
+          url:'/users',
+          data: {
+              username: this.username,
+              password: this.password
+          }
+      }).then(function(response){
+          console.log(response);
+      })
+  }
+
+
+  }])
