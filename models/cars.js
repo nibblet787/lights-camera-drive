3d254ce6ca90d6019b67c723e0f37eb7266a0c6e
Rick Christenhusz <rick.christenhusz@gmail.com>
2018-10-27 23:43:07 -0400

    Removed array from Tags again and changed index to display as String

--- models/cars.js
+++ models/cars.js
@@ -4,7 +4,7 @@ const carSchema = new mongoose.Schema({
   model:{type: String, required:true},
   year:{type: Number, default: 0},
   color:{type: String},
-  tags:[{type: String}],
+  tags:{type: String},
   image:{type: String, default: 'https://driveshare.com/files/cars_images/800/1513303300_phpTs9IkF.jpg'},
   notes:{type: String},
   availability:{type: Boolean, default: false}
