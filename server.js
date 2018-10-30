94e04a09bc37b224f82f6fe2faab6640ce0a76d7
Rick Christenhusz <rick.christenhusz@gmail.com>
2018-10-30 16:11:13 -0400

    changed server.js to push to heroku

--- server.js
+++ server.js
@@ -21,21 +21,21 @@ const session = require('express-session');
 // =======================================
 // To work on LOCAL comment out these next 5 lines
 // =======================================
-// app.use(session({
-//     secret: process.env.SECRET || process.env.HEROKU_SECRET,
-//     resave: false,
-//     saveUninitialized: false
-// }));
+app.use(session({
+    secret: process.env.SECRET || process.env.HEROKU_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
 
 
 // ===================================================
 // To work on LOCAL comment back in these next 5 lines
 // ===================================================
-app.use(session({
-    secret:'feedmeseymour',
-    resave: false,
-    saveUninitialized: false
-}));
+// app.use(session({
+//     secret:'feedmeseymour',
+//     resave: false,
+//     saveUninitialized: false
+// }));
 // =======================================
 //              PORT
 // =======================================
